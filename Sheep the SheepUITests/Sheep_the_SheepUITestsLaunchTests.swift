0f35cb9b95bb82bf04f181d493b34ae043ad4f28
Leon Kwan <kwan.leon@gmail.com>
2023-12-14 11:17:10 -0800

    Initial Commit

---
+++ Sheep the SheepUITests/Sheep_the_SheepUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sheep_the_SheepUITestsLaunchTests.swift
+//  Sheep the SheepUITests
+//
+//  Created by Leon Kwan on 12/14/23.
+//
+
+import XCTest
+
+final class Sheep_the_SheepUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
